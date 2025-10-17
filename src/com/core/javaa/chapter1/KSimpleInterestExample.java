8bb40ce617f89ccab97f30c5322602c9c224daad
universalcodes <yogeshkamra25@gmail.com>
2025-10-17 21:46:34 +0530

    Day 5 - Added Program of Simple Interest and Even Odd.

--- src/com/core/javaa/chapter1/KSimpleInterestExample.java
+++ src/com/core/javaa/chapter1/KSimpleInterestExample.java
@@ -8,20 +8,9 @@ public class KSimpleInterestExample {
         int siResult = principalAmt * rateOfInterest * time /100;  // as per math's result 46.80 but integer stores only non-decimal value so the result is 46
         // To get exact result we need to use decimal data type then result would be 46.80
         System.out.println(siResult);
-
-        double time2 = 5.0;  // It is
+        double time2 = 5.0;  // It is Decimal value
         double siResult2 = principalAmt * rateOfInterest * time2; // Here we  use decimal data type then result would be 46.80
         System.out.println(siResult2);
-
-
-
-
-
-
-        /*
-        Points to remember
-
-                 */
 
     }
 }
