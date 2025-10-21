df3dc98b0dc27bf68c648b7315c411f5c1d52568
universalcodes <yogeshkamra25@gmail.com>
2025-10-21 23:32:43 +0530

    Day 9 - Operators Concept Added - arithmetic Basics Added

---
+++ src/com/core/javaa/chapter2/operators/AArithmeticOperatorr.java
@@ -0,0 +1,30 @@
+package com.core.javaa.chapter2.operators;
+
+public class AArithmeticOperatorr {
+    public static void main(String[] args) {
+
+        /*
+        Points to Remember
+        1. Arithmetic operator is used to perform arithmetic operations addition, subtraction, multiplication, divide & Modulo.
+        2. Addition is used to perform to addition of different Numbers  -- 4+6
+        3. Subtraction is used to perform to subtraction of different Numbers  -- 8+9
+        4. Multiplication is used to perform to Multiplication of different Numbers  -- 6*7
+        5. Divide is used to perform to divide of different Numbers  -- 10/2
+        6. Modulo is used to perform remainder of the number -- 10%2
+        7 If Dividend is greater than divisor then the value of Modulo is Dividend only -- for example 2%10 Here Modulo is 2
+        Another Example 10/15 then Modulo is 10
+        
+        
+        
+         */
+        System.out.println(6+7);  // Perform Addition Operation directly but doesn't store any value.
+        System.out.println(7-5);  // Perform Subtraction Operation directly but doesn't store any value.
+        System.out.println(7*7);  // Perform multiplication Operation directly but doesn't store any value.
+        System.out.println(10/2);  // Perform Divide Operation directly but doesn't store any value.
+        System.out.println(10%2);  //Perform Modulo Operation means stores remainder directly but doesn't store any value.
+        System.out.println(2%10); //Perform Modulo Operation means stores remainder directly but doesn't store any value & Modulo will be 2
+        System.out.println(10%15); //Perform Modulo Operation means stores remainder directly but doesn't store any value & Modulo will be 10
+
+    }
+
+}
